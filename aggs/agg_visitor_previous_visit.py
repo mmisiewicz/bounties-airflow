cf47ba2f0df0f7106134e9b4f09a175c0c4256f0
Michael Misiewicz <mmisiewicz@gmail.com>
2019-02-06 21:06:19 -0500

    bugfixes

--- aggs/agg_visitor_previous_visit.py
+++ aggs/agg_visitor_previous_visit.py
@@ -12,16 +12,16 @@ YDY = """{{ prev_execution_date.strftime("%Y") }}"""
 YDM = """{{ prev_execution_date.strftime("%m") }}"""
 YDD = """{{ prev_execution_date.strftime("%d") }}"""
 # Day before Yesterday ("yesterday yesterday")
-YYDY = """{{ (execution_date - macros.timedelta(days = 1)).strftime("%Y") }}"""
-YYDM = """{{ (execution_date - macros.timedelta(days = 1)).strftime("%m") }}"""
-YYDD = """{{ (execution_date - macros.timedelta(days = 1)).strftime("%d") }}"""
+YYDY = """{{ (execution_date - macros.timedelta(days = 2)).strftime("%Y") }}"""
+YYDM = """{{ (execution_date - macros.timedelta(days = 2)).strftime("%m") }}"""
+YYDD = """{{ (execution_date - macros.timedelta(days = 2)).strftime("%d") }}"""
 
-MACROS = {'YDY': YDY, 'YDM': YDM, 'YDD', YDD, 'YYDY': YYDY, 'YYDM': YYDM, 'YYDD': YYDD}
+MACROS = {'YDY': YDY, 'YDM': YDM, 'YDD': YDD, 'YYDY': YYDY, 'YYDM': YYDM, 'YYDD': YYDD}
 # SQL Command to check for a closed day
 sense_sql = """
 SELECT * from staging.user_page_log
 WHERE
-    time_received >= '%(lhdy)s-%(lhdm)s-%(lhdd)s 23:00:00'
+    time_received >= '%(YDY)s-%(YDM)s-%(YDD)s 23:00:00'
 """
 
 default_args = {
@@ -37,7 +37,7 @@ default_args = {
 
 dag = DAG('agg_visitor_previous_visit', default_args=default_args, schedule_interval= '@daily')
 
-insert_cmd = """INSERT INTO agg_visitor_previous_visit (user_id_uuid, user_id, has_wallet, last_visit, pageviews, ymd)
+insert_cmd = """INSERT INTO agg_visitor_previous_visit (user_id_uuid, user_id, ymd, has_wallet, pageviews, last_visit)
 with daily_pageviews as (select user_id_uuid, user_id, max(has_wallet::int)::boolean as has_wallet,
                          count(*) as pageviews
                          from staging.user_page_log where date(time_received) = '%(YDY)s-%(YDM)s-%(YDD)s'
@@ -46,7 +46,7 @@ with daily_pageviews as (select user_id_uuid, user_id, max(has_wallet::int)::boo
 all_past_pageviews as (select user_id_uuid, user_id, max(date(time_received)) as ymd from staging.user_page_log
                        where date(time_received) <= '%(YYDY)s-%(YYDM)s-%(YYDD)s'
                        group by user_id_uuid, user_id)
-select daily_pageviews.user_id_uuid, daily_pageviews.user_id, '%(YDY)s-%(YDM)s-%(YDD)s' as ymd, has_wallet, pageviews,
+select daily_pageviews.user_id_uuid, daily_pageviews.user_id, '%(YDY)s-%(YDM)s-%(YDD)s'::date as ymd, has_wallet, pageviews,
 coalesce(all_past_pageviews.ymd, '-Infinity'::date) as last_visit
 from daily_pageviews left join all_past_pageviews on daily_pageviews.user_id_uuid = all_past_pageviews.user_id_uuid
 """
